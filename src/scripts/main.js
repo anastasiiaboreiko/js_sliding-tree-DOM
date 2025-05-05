b7ab7a1d68867a24b3661af70b813d3ae7874ba6
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-05-05 16:29:54 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const liElements = document.querySelectorAll('li');
+
+liElements.forEach((li) => {
+  const firstChild = li.firstChild;
+
+  if (firstChild.nodeType === 3 && firstChild.textContent.trim() !== '') {
+    const span = document.createElement('span');
+
+    span.textContent = firstChild.textContent.trim();
+    li.replaceChild(span, firstChild);
+
+    span.addEventListener('click', (e) => {
+      const nestedUl = li.querySelector(':scope > ul');
+
+      if (nestedUl) {
+        nestedUl.hidden = !nestedUl.hidden;
+      }
+      e.stopPropagation();
+    });
+  }
+});
